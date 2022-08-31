7c8dd3adbc507c901dd085a0c7ca374135994f58
Andres Morales <meliand11@yahoo.com>
2022-08-30 20:51:16 -0400

    Slider update needs  work

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//inserts header and bottom html
+
 $(function loadTopPages() {
     $("header").load("top.html");
 });
@@ -5,3 +7,19 @@ $(function loadTopPages() {
 $(function loadbottomPages() {
     $("footer").load("bottom.html");
 });
+
+// image slider
+
+
+var imgDuration = 5000;
+        var fadeSpeed = 2000;
+        var container = $('#slideshow-container');
+        var curIndex = -1;
+
+        function slideShow() {
+             container + $('img.slides').eq(curIndex).fadeOut(fadeSpeed);
+             container + $('img.slides').eq(curIndex  = curIndex < container.children().length - 1 ? curIndex + 1 : 0).fadeIn(fadeSpeed);
+             setTimeout("slideShow()", imgDuration);
+        }
+
+        slideShow();
