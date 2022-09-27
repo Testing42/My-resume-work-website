dfb5bf82b2f0bfee72de861a79d962b103bfb061
Andres Morales <meliand11@yahoo.com>
2022-09-27 18:29:55 -0400

    programs added to code samples

--- js/scripts.js
+++ js/scripts.js
@@ -38,7 +38,7 @@ function collapse(){
             if (content.style.maxHeight){
             content.style.maxHeight = null;
             } else {
-            content.style.maxHeight = content.scrollHeight + "px";
+            content.style.maxHeight = content.scrollHeight + "200px";
             } 
         });
         }
@@ -52,4 +52,50 @@ function copyToClipboard(element) {
         document.execCommand("copy");
         $temp.remove();
       }
-      
+
+
+//display github users
+      function getProfile(e){
+        e.preventDefault();
+        
+
+        var username = document.getElementById('username').value;
+        if(!username || username == ''){
+            username = 'testing42';
+        }
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function(){
+            if(xhttp.readyState == 4 && xhttp.status == 200){
+                var user = JSON.parse(xhttp.responseText);
+                document.getElementById('profile').innerHTML = `<div class="panel 		panel-default">
+                              <div class="panel-heading">
+                            <h3 class="panel-title">${user.name}</h3>
+                              </div>
+                              <div class="panel-body">
+                                <div class="row">
+                                <div class="col-md-3">
+                                <img src="${user.avatar_url}">
+                                </div>
+                                <div class="col-md-9">
+                                    <span class="label label-primary">Public Repos ${user.public_repos}</span>
+                                    <span class="label label-danger">Public Gists ${user.public_gists}</span>
+                                    <br><br>
+                                    <ul class="list-group">
+                                        <li class="list-group-item">Website: ${user.blog}</li>
+                                        <li class="list-group-item">Email: ${user.email}</li>
+                                    </ul>
+                                    <a class="btn btn-default" target="_blank" href="${user.html_url}">Visit Github</a>
+                                </div>
+                                </div>
+                              </div>
+                        </div>`;
+            }
+        }
+        xhttp.open('GET', 'https://api.github.com/users/'+username, true);
+        xhttp.send();
+    }
+
+    document.getElementById('userForm').addEventListener('submit', getProfile, false);
+
+    
