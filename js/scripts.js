8012ec71b2c72d0a6b7055ee826025431c3bb735
Andres Morales <meliand11@yahoo.com>
2023-04-11 13:10:03 -0400

    trying to commit changes again.

--- js/scripts.js
+++ js/scripts.js
@@ -56,27 +56,7 @@ function collapse() {
     }
   }
   
-  
-/*
-function collapse(){
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
 
-    
-        for (i = 0; i < coll.length; i++) {
-        coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.maxHeight){
-            content.style.maxHeight = null;
-            } else {
-            content.style.maxHeight = content.scrollHeight + "200px";
-            } 
-        });
-        }
-    }
-
-    */
 // Copy button
 function copyToClipboard(element) {
     var $temp = $("<input>");
